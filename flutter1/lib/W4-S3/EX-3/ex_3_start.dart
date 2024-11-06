40bf4f7dc2641a67b4636acc849984af30e9842a
TechTheMoneyTeam <ethanroblox168@gmail.com>
2024-11-06 07:50:50 +0700

    Dart X Flutter

---
+++ flutter1/lib/W4-S3/EX-3/ex_3_start.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+List<String> images = [
+  "assets/w4-s2/bird.jpg",
+  "assets/w4-s2/bird2.jpg",
+  "assets/w4-s2/insect.jpg",
+  "assets/w4-s2/girl.jpg",
+  "assets/w4-s2/man.jpg",
+];
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false, // Hides the debug banner in the top-right corner.
+      home: ImageViewer(),
+    ));
+
+class ImageViewer extends StatefulWidget {
+  @override
+  ImageViewerState createState() => ImageViewerState();
+}
+
+class ImageViewerState extends State<ImageViewer> {
+  int currentIndex = 0;
+
+
+  void nextImage() {
+    setState(() {
+      currentIndex = (currentIndex + 1) % images.length;
+    });
+  }
+
+
+  void previousImage() {
+    setState(() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        title: const Text('Image Viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.navigate_before),
+            tooltip: 'Go to the previous image',
+            onPressed: previousImage, 
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Go to the next image',
+              onPressed: nextImage,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Image.asset(images[currentIndex]), 
+      ),
+    );
+  }
+}
